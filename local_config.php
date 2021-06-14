69c0fcc5d6c4ef2d61b68091984880c71f7998ac
ntbchung2401 <85856695+ntbchung2401@users.noreply.github.com>
2021-06-14 11:26:43 +0700

    new modules

---
+++ local_config.php
@@ -0,0 +1,10 @@
+<?php 
+			# DATABASE credential
+			$host = "ec2-54-91-188-254.compute-1.amazonaws.com";
+			$db_name = "d1fhn10tgiktr8";
+			$db_user = "jpcdmyowvfbapw";
+			$db_pwd = "ebe5196a5def33d552614e870e0a6c62bbbd9abe83d5137e07e75ec9e12fa51c";
+			# Create connection to Heroku Postgres
+			$conn_string = "host=$host port=5432 dbname=$db_name user=$db_user password=$db_pwd";
+			
+		?>
