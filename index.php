e9e9d6637facb5f806cc1200eaf86fafe01269b8
ntbchung2401 <85856695+ntbchung2401@users.noreply.github.com>
2021-06-14 09:51:29 +0700

    Create index.php
    for testing

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang ="en">
+	<head>
+		<meta charset = "UTF-8">
+		<title> Welcome to PHP for Cloud app! </title>
+	</head>
+	<body>
+		<?php
+			echo "<h1> Hello cloud computing class </h1>";
+		?>
+	</body>
+</html>
