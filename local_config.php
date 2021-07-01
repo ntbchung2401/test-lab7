d9a3312c735c2448473b03604a76ab461b2d630f
ntbchung2401 <85856695+ntbchung2401@users.noreply.github.com>
2021-07-01 14:42:55 +0700

    update local config.php

--- local_config.php
+++ local_config.php
@@ -1,9 +1,9 @@
 <?php 
 			# DATABASE credential
-			$host = "ec2-54-91-188-254.compute-1.amazonaws.com";
-			$db_name = "d1fhn10tgiktr8";
-			$db_user = "jpcdmyowvfbapw";
-			$db_pwd = "ebe5196a5def33d552614e870e0a6c62bbbd9abe83d5137e07e75ec9e12fa51c";
+			$host = "ec2-52-205-3-3.compute-1.amazonaws.com";
+			$db_name = "d5v9rsbhh67mra";
+			$db_user = "rrkvuhmklhogvq";
+			$db_pwd = "e51a0c26fedf55961589ca88296491d00c07976ef47b10d77d475ac0608ab0d6";
 			# Create connection to Heroku Postgres
 			$conn_string = "host=$host port=5432 dbname=$db_name user=$db_user password=$db_pwd";
 			
