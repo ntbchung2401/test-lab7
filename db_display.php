6d64af7257c2ce2be44afbf5150896859f10cea2
ntbchung2401 <85856695+ntbchung2401@users.noreply.github.com>
2021-07-01 14:48:00 +0700

    update db_display.php

--- db_display.php
+++ db_display.php
@@ -1,36 +1,46 @@
-<?php
-# ardument: query results
-	function display_table($query_obj)
-	{
-		$num_rows = pg_num_rows($query_obj);
-		$num_fields = pg_num_fields($query_obj);
-		echo ' <table border="1">
-				<tr>';
-		$field_list = array();
-		# Display Header
-		for ($i=0; $i<$num_fields; $i++)
-		{
-			$field_name = pg_field_name($query_obj, $i);
-			$field_list[$i] = $field_name;
-			echo "<th> $field_name </th>";		
-		}		
-			echo "</tr>";
-			# display a rows function
-			function display_row($row, $fieldlist)
+<html>
+	<head>
+		<title>Display</title>
+		
+	</head>
+	<body>
+		<?php
+			# ardument: query results
+			function display_table($query_obj)
 			{
-				echo "<tr>\n";
-				foreach ($fieldlist as $fieldname)
+				$num_rows = pg_num_rows($query_obj);
+				$num_fields = pg_num_fields($query_obj);
+				echo ' <table align="center" border="1">
+						<tr>';
+				$field_list = array();
+				# Display Header
+				for ($i=0; $i<$num_fields; $i++)
 				{
-					echo "<td>", $row[$fieldname], "</td>";					
-				}
+					$field_name = pg_field_name($query_obj, $i);
+					$field_list[$i] = $field_name;
+					echo "<th> $field_name </th>";		
+				}		
 				echo "</tr>";
+				# display a rows function
+				function display_row($row, $fieldlist)
+				{
+					echo "<tr>\n";
+					foreach ($fieldlist as $fieldname)
+					{
+						echo "<td>", $row[$fieldname], "</td>";					
+					}
+					echo "</tr>";
+				}
+				# display all rows
+				for ($row_index=0; $row_index<$num_rows; $row_index++)
+				{
+					$row = pg_fetch_array($query_obj, $row_index);
+					# display a row	
+						display_row($row, $field_list);					
+				}
+				echo "</table>";		
 			}
-			# display all rows
-			for ($row_index=0; $row_index<$num_rows; $row_index++)
-			{
-				$row = pg_fetch_array($query_obj, $row_index);
-				# display a row	
-					display_row($row, $field_list);					
-			}
-			echo "</table>";
-?>
+		?>
+	</body>
+
+</html>
